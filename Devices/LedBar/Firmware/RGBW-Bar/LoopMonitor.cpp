17db0259124dc2fbef13561fabfd5ff19a9b5ed6
Roel Drost <roelpuntdrost@gmail.com>
2023-12-30 21:29:38 +0100

    Reset Loop monitor each get

--- Devices/LedBar/Firmware/RGBW-Bar/LoopMonitor.cpp
+++ Devices/LedBar/Firmware/RGBW-Bar/LoopMonitor.cpp
@@ -8,17 +8,24 @@ static long lps_count_last = 0;
 static long lps_count_min = 0;
 static long lps_count_max = 0;
 
+static void LoopMonitorReset();
+
 void LoopMonitorBegin() {
-  lps_count          = 0;
+  LoopMonitorReset();
 }
 
 void LoopMonitorLoop() {
   lps_count++;
-  if (CronEvery1Second()) {
-    lps_count_last = lps_count;
-    if (lps_count_min == 0 || lps_count_min > lps_count) lps_count_min = lps_count;
-    if (lps_count_max < lps_count) lps_count_max = lps_count;
+  if (CronEvery4thSecond()) {
+    lps_count_last = lps_count * 4;
     lps_count          = 0;
+    
+    if (lps_count_min == 0 || lps_count_min > lps_count_last) {
+      lps_count_min = lps_count_last;
+    }
+    if (lps_count_max < lps_count_last) {
+      lps_count_max = lps_count_last;
+    }
   }
 }
 
@@ -26,5 +33,13 @@ void LoopMonitorGet(long &last, long &min, long &max) {
   last = lps_count_last;
   min  = lps_count_min;
   max  = lps_count_max;
+  LoopMonitorReset();
+}
+
+static void LoopMonitorReset() {
+  lps_count = 0;
+  lps_count_last = 0;
+  lps_count_min = 0;
+  lps_count_max = 0;
 }
 
